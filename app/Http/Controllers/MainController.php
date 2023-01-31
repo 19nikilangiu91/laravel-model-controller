7883cc4e2282f8b79ad172ed36e55f611af71c32
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-31 16:33:30 +0100

    Added Content

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,12 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//7) Importo il model Movies.php.
+
+use App\Models\Movies;
+
 class MainController extends Controller
 {
     // 1) Creaiamo una funzione "Home" ed andiamo a collegarla alla route "web.php".
     public function home()
     {
         // 4) Una volta create la page "home.blade.php", imposto il return.
-        return view('pages.home');
+        // return view('pages.home');
+
+        // 5) Creo un $data di prova
+
+        // $data = [
+        //     'name' => 'Niki'
+        // ];
+
+        // 8) Richiamo la variabile $movies.
+
+        $movies = Movies::all();
+
+        $data = [
+            'movies' => $movies
+        ];
+
+        // var_dump($data);
+
+        return view('pages.home', $data);
     }
 }
