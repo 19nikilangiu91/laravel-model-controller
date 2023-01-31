ecbc3b00dde2ff982cf11c22dc020aff7a3b30f1
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-31 17:32:07 +0100

    Update with get and set

--- app/Models/Movies.php
+++ app/Models/Movies.php
@@ -11,10 +11,60 @@ class Movies extends Model
 
     // 6) Creo le variabili che rappresentano le colonne di "movies" nel DB "movie_db"
 
-    public $id;
-    public $title;
-    public $original_title;
-    public $nationality;
-    public $date;
-    public $vote;
+    private $id;
+    private $title;
+    private $original_title;
+    private $nationality;
+    private $date;
+    private $vote;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+    public function getOriginal_title()
+    {
+        return $this->original_title;
+
+    }
+    public function setOriginal_title($original_title)
+    {
+        $this->original_title = $original_title;
+    }
+    public function getNationality()
+    {
+        return $this->nationality;
+    }
+    public function setNationality($nationality)
+    {
+        $this->nationality = $nationality;
+    }
+    public function getDate()
+    {
+        return $this->date;
+    }
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+    public function getVote()
+    {
+        return $this->vote;
+    }
+    public function setVote($vote)
+    {
+        $this->vote = $vote;
+    }
 }
